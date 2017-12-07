9ed407f7566570827932ed638b3698b06f724892
אלון שץ <alonshats49@gmail.com>
2017-12-07 18:03:45 +0200

    no token

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ var express = require('express');
 var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
 var db = require('./db');
+var csrf = require('csurf');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser')
 
-
+var Tokens = require('csrf');
+var token = Tokens();
+var secret = token.secretSync();
+var csrfToken = "aaa";
 // Configure the local strategy for use by Passport.
 //
 // The local strategy require a `verify` function which receives the credentials
@@ -11,14 +17,20 @@ var db = require('./db');
 // that the password is correct and then invoke `cb` with a user object, which
 // will be set at `req.user` in route handlers after authentication.
 passport.use(new Strategy(
-  function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
-    });
-  }));
+    function (username, password, cb) {
+        db.users.findByUsername(username, function (err, user) {
+            if (err) {
+                return cb(err);
+            }
+            if (!user) {
+                return cb(null, false);
+            }
+            if (user.password != password) {
+                return cb(null, false);
+            }
+            return cb(null, user);
+        });
+    }));
 
 
 // Configure Passport authenticated session persistence.
@@ -28,23 +40,28 @@ passport.use(new Strategy(
 // typical implementation of this is as simple as supplying the user ID when
 // serializing, and querying the user record by ID from the database when
 // deserializing.
-passport.serializeUser(function(user, cb) {
-  cb(null, user.id);
+passport.serializeUser(function (user, cb) {
+    cb(null, user.id);
 });
 
-passport.deserializeUser(function(id, cb) {
-  db.users.findById(id, function (err, user) {
-    if (err) { return cb(err); }
-    cb(null, user);
-  });
+passport.deserializeUser(function (id, cb) {
+    db.users.findById(id, function (err, user) {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, user);
+    });
 });
-
-
 
 
 // Create a new Express application.
 var app = express();
-
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded());
 // Configure view engine to render EJS templates.
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -53,8 +70,8 @@ app.set('view engine', 'ejs');
 // logging, parsing, and session handling.
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(require('body-parser').urlencoded({extended: true}));
+app.use(require('express-session')({secret: 'keyboard cat', resave: false, saveUninitialized: false}));
 
 // Initialize Passport and restore authentication state, if any, from the
 // session.
@@ -63,31 +80,68 @@ app.use(passport.session());
 
 // Define routes.
 app.get('/',
-  function(req, res) {
-    res.render('home', { user: req.user });
-  });
+    function (req, res) {
+        res.render('home', {user: req.user});
+    });
 
 app.get('/login',
-  function(req, res){
-    res.render('login');
-  });
-  
-app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
-  
+    function (req, res) {
+        res.render('login');
+    });
+
+app.post('/login',
+    passport.authenticate('local', {failureRedirect: '/login'}),
+    function (req, res) {
+        res.redirect('/');
+    });
+app.get('/register',
+    function (req, res) {
+        res.render('register');
+    });
+app.post('/register',
+    function (req, res) {
+        var userName = req.body.username;
+        var password = req.body.password;
+
+        var Tokens = require('csrf');
+        var token = Tokens();
+        var secret = token.secretSync();
+        var csrfToken = token.create(secret);
+        db.users.addUser(userName, password, csrfToken);
+        res.redirect('/login');
+    });
+
 app.get('/logout',
-  function(req, res){
-    req.logout();
-    res.redirect('/');
-  });
+    function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
 
 app.get('/profile',
-  require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res){
-    res.render('profile', { user: req.user });
-  });
+    require('connect-ensure-login').ensureLoggedIn(),
+    function (req, res) {
+        res.render('profile', {user: req.user});
+    });
+app.get('/transfer',
+    require('connect-ensure-login').ensureLoggedIn(),
+    function (req, res) {
+        res.render('transferring', {csrfSecret: req.user.csrfSecret});
+    });
+
+app.post('/transfer', function (req, res) {
+    console.log(req.user);
+    // console.log(req.body._csrf);
+    // var csrft = req.body._csrf;
+    var bodyStr = '';
+    req.on("data", function (chunk) {
+        bodyStr += chunk.toString();
+        console.log(bodyStr);
+        var splited = bodyStr.split("&");
+        var csrft = splited[0].split("=")[1]
+        if (req.user.csrfSecret === csrft)
+            console.log(data.toString());
+    });
+});
+
 
 app.listen(3000);
